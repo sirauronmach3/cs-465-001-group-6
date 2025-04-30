43f41d501ee160edab71cb75b75b03774e0905be
Matthew <cyrilmartinmj@gmail.com>
2025-04-30 16:36:12 -0400

    fixing the automation command

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ SERVER="mjm836@tux.cs.drexel.edu"
 DIRECTORY="~/public_html/cs-465-001-group-6"
 
 # Connect to server and execute commands
-ssh $SERVER "cd $DIRECTORY && git pull & chmod 644 *.html"
+ssh $SERVER "cd $DIRECTORY && git pull && chmod 644 *.html"
 
 # Add error handling
 if [ $? -eq 0 ]; then
